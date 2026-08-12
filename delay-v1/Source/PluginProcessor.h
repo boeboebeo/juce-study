db6129fca6acb2e2698f0bbb84869e35afab217a
boe <boeboebeo@gmail.com>
2026-08-12 23:37:21 +0900

    feat: 생성자, 소멸자 호출

--- delay-v1/Source/PluginProcessor.h
+++ delay-v1/Source/PluginProcessor.h
@@ -1,2 +1,15 @@
 #pragma once
 #include <juce_audio_processors/juce_audio_processors.h>
+
+class DelayAudioProcessor : public juce::AudioProcessor
+{
+public:
+    DelayAudioProcessor();
+    ~DelayAudioProcessor() override = default;
+}
+
+    //DAW가 재생 시작 직전 1회 호출 -> 여기서 버퍼/스무딩 값 준비
+void prepareToPlay(double sampleRate, int samplesPerBlock) override;
+void releaseResources() override;
+
+
