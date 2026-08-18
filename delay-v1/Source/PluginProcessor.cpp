e7cfc0f380f03845465588ab890e9c542f3330b8
boe <boeboebeo@gmail.com>
2026-08-19 01:43:15 +0900

    feat: feedback, delay time, mix 값 Push_back

--- delay-v1/Source/PluginProcessor.cpp
+++ delay-v1/Source/PluginProcessor.cpp
@@ -20,5 +20,30 @@ juce::AudioProcessorValueTreeState::ParameterLayout DelayAudioProcessor::createP
 {
     std::vector<std::unique_ptr<juce::RangedAudioParameter>> params;
     
-    //
+    //1) Delay time 0 ~ 2000ms 까지 사용할 수 있도록 셋팅 (버퍼도 그만큼 크게 준비함)
+    params.push_back(std::make_unique<juce::AudioParameterFloat>(
+         juce::ParameterID {"delayTime", 1},
+         "Delay Time",
+         juce::NormalisableRange<float>(1.0f, 2000.0f, 0.1f, 0.3f),
+         300.0f,
+         "ms"
+                                                                 ));
+    
+    //2) Feedback (1.0 이상이면 소리가 무한 증폭디므로, 0.98까지만 노출 (안전장치))
+    params.push_back(std::make_unique<juce::AudioParameterFloat>(
+         juce::ParameterID { "feedback", 1 },
+         "Feedback",
+         juce::NormalisableRange<float>(0.0f, 0.98f, 0.01f),
+         0.3f
+                                                                 ));
+    
+    //3) Mix
+    params.push_back(std::make_unique<juce::AudioParameterFloat>(
+         juce::ParameterID { "mix", 1 },
+         "Mix",
+         juce::NormalisableRange<float>(0.0f, 1.0f, 0.01f),
+         0.5f
+                                                                 ));
+    
+    return { params.begin(), params.end() };
 }
