e84fdec0be14c83586581918b981acf77ddbc593
boe <boeboebeo@gmail.com>
2026-09-18 02:42:14 +0900

    feat: push_back study, RangedAudioParameterFloat

--- biquadeq-v1/Source/PluginProcessor.cpp
+++ biquadeq-v1/Source/PluginProcessor.cpp
@@ -19,5 +19,39 @@ ThreeBandEQAudioProcessor::~ThreeBandEQAudioProcessor() = default;
 
 
 //=============================================================
-
-
+juce::AudioProcessorValueTreeState::ParameterLayout ThreeBandEQAudioProcessor::createParameterLayout()
+{
+    std::vector<std::unique_ptr<juce::RangedAudioParameter>> params;
+    
+    //High-pass band
+    params.push_back (std::make_unique<juce::AudioParameterFloat> (
+                   "hpFreq", //Parameter ID (첫번째 인자로 전달되는 문자열이 파라미터 식별자)
+                   "HP Freq",   //사용자에게 보여주긴 위한 파라미터 이름
+                   juce::NormalisableRange<float> (20.0f, 2000.0f, 0.1f, 0.3f), 100.0f));
+    params.push_back (std::make_unique<juce::AudioParameterFloat> (
+                   "hpQ",
+                   "HP Q",
+                   juce::NormalisableRange<float> (0.1f, 10.0f, 0.01f, 0.5f), 0.707f
+                                                                   ));
+    
+        //위의 AudioParameterFloat 을 params 가 소유함 (이 객체의 유일한 소유자)
+        //그래서 delete 할 필요 없음
+    
+    params.push_back (std::make_unique<juce::AudioParameter<float>>(
+                    "peakFreq",
+                    "Peak Freq",
+                    juce::NormalisableRange<float> (20.0f, 20000.0f, 0.1f, 0.3f), 100.0f
+                                                                    ));
+    params.push_back (std::make_unique<juce::AudioParameterFloat> (
+                   "peakQ",
+                   "Peak Q",
+                   juce::NormalisableRange<float> (0.1f, 10.0f, 0.01f, 0.5f), 1.0f
+                                                                   ));
+    params.push_back (std::make_unique<juce::AudioParameterFloat>(
+                  "peakGain",
+                  "Peak Gain",
+                  juce::NormalisableRange<float> (-24.0f, 24.0f, 0.1f), 6.0f
+                                                                  ));
+    
+    
+}
